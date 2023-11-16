68f7cfcd1bc2e1605ed14ebd5745ce2269ac36bc
Gnoyh <4ksenhs@gmail.com>
2023-11-16 10:35:36 +0900

    commit 181933

---
+++ ps_181933.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181933
+
+def solution(a, b, flag):
+    if flag:
+        answer = a + b
+    else:
+        answer = a - b
+
+    return answer
