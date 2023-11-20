59ca6f50b32267bd169b3b418d87b39787b0ff5e
Gnoyh <4ksenhs@gmail.com>
2023-11-20 14:04:59 +0900

    commit 181930

--- ps_181930.py
+++ ps_181930.py
@@ -1,13 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/181930
 
 def solution(a, b, c):
-    answer = a + b + c
+    check_list = [a, b, c]
 
-    if a == b and a == c:
-        answer *= (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3)
-    elif a != b and a != c and b != c:
-        return answer
+    if len(set(check_list)) == 3:
+        return a + b + c
+    elif len(set(check_list)) == 2:
+        return (a + b + c) * (a ** 2 + b ** 2 + c ** 2)
     else:
-        answer *= (a ** 2 + b ** 2 + c ** 2)
-
-    return answer
+        return a * (a ** 2) * (a ** 3) * 27
