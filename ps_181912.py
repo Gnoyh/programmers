387d680713f4379ef6bcaeaeac1b78f781338bf6
Gnoyh <4ksenhs@gmail.com>
2023-11-23 09:42:30 +0900

    commit 181912

--- ps_181912.py
+++ ps_181912.py
@@ -1,2 +1,10 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/181912
 
+def solution(intStrs, k, s, l):
+    result_list = []
+
+    for i in intStrs:
+        if int(i[s: s + l]) > k:
+            result_list.append(int(i[s: s + l]))
+
+    return result_list
