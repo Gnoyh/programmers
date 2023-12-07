e3003c12600d693c69b55409e33b56e107e8ce0b
Gnoyh <4ksenhs@gmail.com>
2023-12-07 22:40:04 +0900

    commit 181851

---
+++ ps_181851.py
@@ -0,0 +1,6 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181851
+
+def solution(rank, attendance):
+    a, b, c = sorted([(v, i) for i, v in enumerate(rank) if attendance[i]])[: 3]
+    
+    return 10000 * a[1] + 100 * b[1] + c[1]
