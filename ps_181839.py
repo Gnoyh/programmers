8fde3425bfaa4eab6180e8df09aaf47222fc54fd
Gnoyh <4ksenhs@gmail.com>
2023-12-10 08:16:01 +0900

    commit 181839

---
+++ ps_181839.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181839
+
+def solution(a, b):
+    if a % 2 == 1 and b % 2 == 1:
+        return a ** 2 + b ** 2
+    elif a % 2 == 1 or b % 2 == 1:
+        return 2 * (a + b)
+    else:
+        return abs(a - b)
