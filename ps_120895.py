d80e7cf16880141dc528bb45727e9ffc1f10f6ff
Gnoyh <4ksenhs@gmail.com>
2023-12-14 07:29:44 +0900

    commit 120895

---
+++ ps_120895.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120895
+
+def solution(my_string, num1, num2):
+    result = list(my_string)
+    
+    result[num1], result[num2] = result[num2], result[num1]
+    
+    return "".join(result)
