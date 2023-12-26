b92868522eb5e0f4b9b4b6b8086133689d1044e8
Gnoyh <4ksenhs@gmail.com>
2023-12-26 19:11:40 +0900

    commit120880

---
+++ ps_120880.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120880
+
+def solution(numlist, n):
+    numlist = [(abs(i - n), i) for i in numlist]
+    
+    numlist.sort(key = lambda x: (x[0], -x[1]))
+    
+    return [i[1] for i in numlist]
