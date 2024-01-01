b25a326efa05fe08488f146a2fcefdbaaf3aeb4d
Gnoyh <4ksenhs@gmail.com>
2024-01-01 21:55:45 +0900

    commit 120860

---
+++ ps_120860.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/120860
+
+def solution(dots):
+    x_list = [i[0] for i in dots]
+    y_list = [i[1] for i in dots]
+    
+    return (max(x_list) - min(x_list)) * (max(y_list) - min(y_list))
