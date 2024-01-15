0de6b31a6d3d497abe4e73c67b89878dfe7da462
Gnoyh <4ksenhs@gmail.com>
2024-01-15 09:18:04 +0900

    commit 155652

---
+++ ps_155652.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/155652
+
+def solution(s, skip, index):
+    result = ""
+    alphabet_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    
+    for i in skip:
+        alphabet_list.remove(i)
+        
+    for i in s:
+        result += alphabet_list[(alphabet_list.index(i) + index) % len(alphabet_list)]
+        
+    return result
