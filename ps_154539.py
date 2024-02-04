982c3ab5218e32a1366ca0505afd959c3c5b3520
Gnoyh <4ksenhs@gmail.com>
2024-02-04 21:27:35 +0900

    commit 154539

---
+++ ps_154539.py
@@ -0,0 +1,20 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/154539
+
+def solution(numbers):
+    result = [-1]
+    
+    max_list = [numbers[-1]]
+    
+    for i in range(len(numbers) - 2, -1, -1):
+        if numbers[i] < max_list[0]:
+            while numbers[i] >= max_list[-1]:
+                max_list.pop()
+                
+            result.append(max_list[-1])
+            max_list.append(numbers[i])
+        else:
+            max_list = [numbers[i]]
+            
+            result.append(-1)
+            
+    return result[: : -1]
