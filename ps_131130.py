347d5d183c9e8e8019c938874e3421ad3cf9b231
Gnoyh <4ksenhs@gmail.com>
2024-02-11 15:49:22 +0900

    commit 131130

---
+++ ps_131130.py
@@ -0,0 +1,27 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/131130
+
+def solution(cards):
+    result_list = []
+    
+    cards_dict = {}
+    
+    for i in range(len(cards)):
+        cards_dict[i + 1] = cards[i]
+        
+    for i in range(1, len(cards) + 1):
+        before_idx = 0
+        idx = i
+        count = 0
+        
+        while cards_dict[idx] != 0:
+            before_idx = idx
+            idx = cards_dict[idx]
+            cards_dict[before_idx] = 0
+            
+            count += 1
+            
+        result_list.append(count)
+        
+    result_list.sort()
+    
+    return result_list[-1] * result_list[-2]
