c92aa3f2698675f3c4fab7a805175c60e6154997
Gnoyh <4ksenhs@gmail.com>
2024-02-18 20:06:43 +0900

    commit 84512

--- ps_84512.py
+++ ps_84512.py
@@ -1,2 +1,13 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/84512
 
+def solution(word):
+    result = 0
+    
+    count_list = [(5 ** i - 1) // 4 for i in range(5, 0, -1)]
+    
+    word_dict = {"A": 0, "E": 1, "I": 2, "O": 3, "U": 4}
+    
+    for i, v in enumerate(word):
+        result += count_list[i] * word_dict[v] + 1
+        
+    return result
