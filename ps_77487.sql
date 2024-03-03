25bf24073d7081802bf6128a49670ab84fc0f56f
Gnoyh <4ksenhs@gmail.com>
2024-03-03 19:11:45 +0900

    commit 77487

---
+++ ps_77487.sql
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/77487
+
+WITH VT AS (
+    SELECT *
+    FROM PLACES
+    GROUP BY HOST_ID
+    HAVING COUNT(ID) > 1
+    )
+SELECT P.*
+FROM PLACES AS P
+JOIN VT ON P.HOST_ID = VT.HOST_ID
