01930ee35e49cc09a380e90d6e8fe8ade4a250e9
Gnoyh <4ksenhs@gmail.com>
2024-04-20 22:01:51 +0900

    commit 299305

---
+++ ps_299305.sql
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/299305
+
+SELECT P.ID, COUNT(C.ID) AS CHILD_COUNT
+FROM ECOLI_DATA AS P
+LEFT JOIN ECOLI_DATA AS C ON P.ID = C.PARENT_ID
+GROUP BY P.ID
+ORDER BY P.ID
