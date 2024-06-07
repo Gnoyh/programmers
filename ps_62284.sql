097a0a83b264cc69eae4c55eb2544bc5ae3f5a82
Gnoyh <4ksenhs@gmail.com>
2024-06-07 15:26:10 +0900

    commit 62284

---
+++ ps_62284.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/62284
+
+SELECT CART_ID
+FROM CART_PRODUCTS
+WHERE NAME IN ('Milk', 'Yogurt')
+GROUP BY CART_ID
+HAVING COUNT(DISTINCT NAME) = 2
+ORDER BY CART_ID
