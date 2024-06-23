558a9e866e87398b1b0eed54e6267c9b01382842
Gnoyh <4ksenhs@gmail.com>
2024-06-23 21:11:28 +0900

    commit 298519

---
+++ ps_298519.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/298519
+
+SELECT COUNT(ID) AS FISH_COUNT, MAX(LENGTH) AS MAX_LENGTH, FISH_TYPE
+FROM FISH_INFO
+GROUP BY 3
+HAVING AVG(IFNULL(LENGTH, 10)) >= 33
+ORDER BY 3
