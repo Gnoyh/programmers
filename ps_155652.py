6fd9bf9488241bdd2766f192c075a0b8abf28113
Gnoyh <4ksenhs@gmail.com>
2024-07-23 21:19:34 +0900

    commit 155652

--- ps_155652.py
+++ ps_155652.py
@@ -2,6 +2,7 @@
 
 def solution(s, skip, index):
     result = ""
+    
     alphabet_list = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     
     for i in skip:
