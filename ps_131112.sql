3f9c1a08504c82ebe7ad3930d0460dbfe11f7432
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2024-08-27 22:48:26 +0900

    commit 131112

--- ps_131112.sql
+++ ps_131112.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/131112
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131112
 
 SELECT FACTORY_ID, FACTORY_NAME, ADDRESS
 FROM FOOD_FACTORY
