42ab47960f1d4a720e4a3c2c708e77ea6b97ca11
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2024-08-29 12:15:13 +0900

    commit 59411

--- ps_59411.sql
+++ ps_59411.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/59411
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59411
 
 SELECT AI.ANIMAL_ID, AO.NAME
 FROM ANIMAL_INS AS AI
