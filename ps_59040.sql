56e081a3018f3d315a62f0a1288f5ebd3b259266
Gnoyh <4ksenhs@gmail.com>
2024-09-02 21:28:44 +0900

    commit 59040

--- ps_59040.sql
+++ ps_59040.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/59040
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59040
 
 SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) AS 'count'
 FROM ANIMAL_INS
