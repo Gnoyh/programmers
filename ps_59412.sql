d153395d40128f015211bcaaa2d46f2758027870
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2024-09-05 21:36:07 +0900

    commit 59412

--- ps_59412.sql
+++ ps_59412.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/59412
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59412
 
 SELECT HOUR(DATETIME) AS 'HOUR', COUNT(DATETIME) AS 'COUNT'
 FROM ANIMAL_OUTS
