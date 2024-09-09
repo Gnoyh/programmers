20e8a207a730e7535b35110d60a4cb8659f65fd8
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2024-09-09 22:11:57 +0900

    commit 132201

--- ps_132201.sql
+++ ps_132201.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/132201
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132201
 
 SELECT PT_NAME, PT_NO, GEND_CD, AGE, IFNULL(TLNO, 'NONE') AS TLNO
 FROM PATIENT
