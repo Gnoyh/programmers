ba5b1135895d1a3f3e3d3f34cc22aec8e774ae9d
Gnoyh <4ksenhs@gmail.com>
2024-09-13 17:12:37 +0900

    commit 59043

--- ps_59043.sql
+++ ps_59043.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/59043
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59043
 
 SELECT AI.ANIMAL_ID, AO.NAME
 FROM ANIMAL_INS AS AI
