e0bca026fc98995184937d63f247b14dafc93e9d
Gnoyh <4ksenhs@gmail.com>
2024-09-15 06:36:46 +0900

    commit 59046

--- ps_59046.sql
+++ ps_59046.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/59046
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59046
 
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
