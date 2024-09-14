84e3dbf11b36367c4ae575bb71cf79e2d694d251
Gnoyh <4ksenhs@gmail.com>
2024-09-14 17:46:01 +0900

    commit 59044

--- ps_59044.sql
+++ ps_59044.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/59044
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59044
 
 SELECT AI.NAME, AI.DATETIME
 FROM ANIMAL_INS AS AI
