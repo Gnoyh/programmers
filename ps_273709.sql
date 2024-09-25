b06871e50614b4e38f55299068ed8b56946d1ec9
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2024-09-25 21:57:46 +0900

    commit 273709

--- ps_273709.sql
+++ ps_273709.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/273709
+-- https://school.programmers.co.kr/learn/courses/30/lessons/273709
 
 SELECT SUM(PRICE) AS TOTAL_PRICE
 FROM ITEM_INFO
