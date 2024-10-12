f4fd8ce1feadfafabcd7a09244d27b85127920bf
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2024-10-12 15:16:14 +0900

    commit 131530

--- ps_131530.sql
+++ ps_131530.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/131530
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131530
 
 SELECT FLOOR(PRICE / 10000) * 10000 AS PRICE_GROUP, COUNT(PRICE) AS PRODUCTS
 FROM PRODUCT
