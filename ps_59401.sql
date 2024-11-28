3e4ffeb0e68576a420822fefe85db2a1d9cc7bd4
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2024-11-28 18:34:07 +0900

    commit 59401

--- ps_59401.sql
+++ ps_59401.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/59041
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59041
 
 SELECT NAME, COUNT(NAME) AS 'COUNT'
 FROM ANIMAL_INS
