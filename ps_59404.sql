55edeecd3d8b02a78394e67e3da7f1e248478fa3
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2024-12-01 22:43:09 +0900

    commit 59404

--- ps_59404.sql
+++ ps_59404.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/59404
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59404
 
 SELECT ANIMAL_ID, NAME, DATETIME
 FROM ANIMAL_INS
