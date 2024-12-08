ec759c4510dca5b3bdeee8020a955dc0632a1d56
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2024-12-08 19:08:59 +0900

    commit 276034

--- ps_276034.sql
+++ ps_276034.sql
@@ -1,4 +1,4 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/276034
+-- https://school.programmers.co.kr/learn/courses/30/lessons/276034
 
 SELECT ID, EMAIL, FIRST_NAME, LAST_NAME
 FROM DEVELOPERS
