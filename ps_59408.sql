2577cd65b6abba30ad707dd126f65506950d2e40
Gnoyh <127109635+Gnoyh@users.noreply.github.com>
2025-01-11 13:13:38 +0900

    commit 59408

--- ps_59408.sql
+++ ps_59408.sql
@@ -1,5 +1,5 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/59408
 
-SELECT COUNT(DISTINCT NAME) AS 'count'
+SELECT COUNT(DISTINCT NAME) AS 'COUNT'
 FROM ANIMAL_INS
 WHERE NAME IS NOT NULL
